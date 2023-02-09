f32b2c41c5a609a4981fe6830f0bfd2d6ecf526d
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-02-09 12:08:26 +0530

    added mui

--- src/components/workshopSubPages/aura/AuraBuilder.jsx
+++ src/components/workshopSubPages/aura/AuraBuilder.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Footer from "../../footer/Footer";
+import Button from "@mui/material/Button";
 
 const AuraBuilder = () => {
   return (
     <>
       <div>AuraBuilder</div>
+      <Button variant="contained">hell</Button>
+
       {/* <Footer/> */}
     </>
   );
