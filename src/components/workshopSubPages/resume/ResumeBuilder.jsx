828ffdf5982a569aee6ea4eb155d0a365bba8f95
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-02-10 11:27:05 +0530

    added date

--- src/components/workshopSubPages/resume/ResumeBuilder.jsx
+++ src/components/workshopSubPages/resume/ResumeBuilder.jsx
@@ -1,19 +1,45 @@
-import React from "react";
-import chartH from '../../../images/headers/chart.png'
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import chartH from "../../../images/headers/chart.png";
 import { ResumeStyleDiv } from "./resumeStyle";
 
-const ResumeBuilder = () => {
-  return (
-  <ResumeStyleDiv>
-    <img src={chartH} alt="header" className="header"/>
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import { LocalizationProvider, MobileDatePicker } from "@mui/x-date-pickers";
 
-    <section className="rMain">
-      {/* main div where textfields will b there  */}
+const ResumeBuilder = () => {
+  const [value, setValue] = useState(dayjs("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <ResumeStyleDiv>
+      <img src={chartH} alt="header" className="header" />
+
+      <section className="rMain">
+        {/* main div where textfields will b there  */}
         <div className="left">
           <div className="user-input">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt doloribus dignissimos officiis voluptate possimus neque amet ad saepe obcaecati eveniet. Repudiandae eveniet cum quia voluptatibus sapiente aspernatur excepturi eaque nobis facere fugit iste, quod quasi nulla expedita ea nisi architecto, magnam, dolor tenetur omnis quos placeat inventore? Porro esse iure tempore. Nihil tempora delectus repudiandae nobis dolorem molestiae illum porro, suscipit expedita sapiente, eius ullam possimus adipisci laboriosam alias repellat, voluptatum provident ipsam sit? Placeat suscipit esse necessitatibus magni omnis, alias libero, consequuntur ea laudantium unde eaque dolorem odit eveniet, deserunt cumque adipisci pariatur eius? Velit officiis commodi placeat iure?
-              </p>
+            <TextField label="Name" variant="filled" />
+
+            {/* <DesktopDatePicker
+              label="Date desktop"
+              inputFormat="MM/DD/YYYY"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            /> */}
+            <br />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <MobileDatePicker
+                label="Date mobile"
+                inputFormat="MM/DD/YYYY"
+                value={value}
+                onChange={handleChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
           </div>
         </div>
 
@@ -21,9 +47,8 @@ const ResumeBuilder = () => {
         <div className="right">
           <div className="a4"></div>
         </div>
-    </section>
-    
-  </ResumeStyleDiv>
+      </section>
+    </ResumeStyleDiv>
   );
 };
 
--- src/components/workshopSubPages/resume/resumeStyle.js
+++ src/components/workshopSubPages/resume/resumeStyle.js
@@ -1,82 +1,87 @@
 import styled from "styled-components";
 
-export const ResumeStyleDiv=styled.div`
-    margin-top: -2.5%;
-    img.header{
-        width: 20%;
-        position: absolute;
-        top: 2%;
-        left: 2%;
-    }
-    section.rMain{
-        display: grid;
-        grid-template-columns: 60% 40%;
-        /* border: 1px solid black; */
-        position: absolute;
-        width: 98%;
-        height: 83%;
-        margin-left: 1%;
-        div.left{
-            padding: 2%;
-            background-color: #eeee6f74;
-            overflow: auto;
+export const ResumeStyleDiv = styled.div`
+  margin-top: -2.5%;
+  img.header {
+    width: 20%;
+    position: absolute;
+    top: 2%;
+    left: 2%;
+  }
+  section.rMain {
+    display: grid;
+    grid-template-columns: 60% 40%;
+    /* border: 1px solid black; */
+    position: absolute;
+    width: 98%;
+    height: 83%;
+    margin-left: 1%;
+    div.left {
+      padding: 2%;
+      /* background-color: #eeee6f74; */
+      background-color: #b0b0b09c;
+      overflow: auto;
 
-                ::-webkit-scrollbar {
-                    width: 10px;
-                }
+      ::-webkit-scrollbar {
+        width: 10px;
+      }
 
-                /* Track */
-                ::-webkit-scrollbar-track {
-                    border-radius: 3px;
-                }
-                /* Handle */
-                ::-webkit-scrollbar-thumb {
-                    background: #eaa5a5; 
-                    border-radius: 3px;
-                    
-                }
+      /* Track */
+      ::-webkit-scrollbar-track {
+        border-radius: 3px;
+      }
+      /* Handle */
+      ::-webkit-scrollbar-thumb {
+        background: #eaa5a5;
+        border-radius: 3px;
+      }
 
-            div.user-input{
-                width: 100%;
-                height: 100%;
-                p{
-                    width: 20%;
-                }
-            }
+      div.user-input {
+        width: 100%;
+        height: 100%;
+        p {
+          width: 20%;
         }
-        div.right{
-            background-color: #f965654e;
-            padding: 2%;
-            position: relative;
+      }
+    }
+    div.right {
+      background-color: #f965654e;
+      padding: 2%;
+      position: relative;
 
-            div.a4{
-                width: 70%;
-                margin: auto;
-                height: 98.994%;
-                background-color: #7d79799c;
-            }
-        }
+      div.a4 {
+        /* width: 70px; */
+        width: 385px;
+        margin: auto;
+        margin-top: 3%;
+        /* height: 98.994px; */
+        height: 544.467px;
+        background-color: #7d79799c;
+      }
     }
+  }
 
-
-    /* media queries ####### */
-    @media (max-width:1280px){
-        
+  /* media queries ####### */
+  @media (max-width: 1280px) {
+    section.rMain div.right div.a4 {
+      /* background-color: red; */
+      margin-top: 25px;
     }
-    @media (max-width:820px){
-        img.header{
-            width: 30%;
-            position: absolute;
-            top: 0.3%;
-            left: 1%;
-        }
+  }
+  @media (max-width: 820px) {
+    img.header {
+      width: 30%;
+      position: absolute;
+      top: 0.3%;
+      left: 1%;
     }
-    @media (max-width:520px){
-        img.header{
-            width: 70%;
-            position: relative;
-            margin-left: 12%;
-            margin-top: -5%;
-        }
+  }
+  @media (max-width: 520px) {
+    img.header {
+      width: 70%;
+      position: relative;
+      margin-left: 12%;
+      margin-top: -5%;
     }
+  }
 `;
