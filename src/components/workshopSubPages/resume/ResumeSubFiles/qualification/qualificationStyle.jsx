506712fa4ec43132a76987cc281ca7b5977b035f
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-02-13 19:00:46 +0530

    working on qualification

--- src/components/workshopSubPages/resume/ResumeSubFiles/qualification/qualificationStyle.jsx
+++ src/components/workshopSubPages/resume/ResumeSubFiles/qualification/qualificationStyle.jsx
@@ -2,13 +2,34 @@ import styled from "styled-components";
 
 const QualificationContainer = styled.div`
   .qualification {
+    margin-top: 2%;
+    padding: 1%;
+    padding-bottom: 5%;
     position: relative;
     border: 1px solid black;
     border-radius: 5px;
-  }
-
-  .qualification-label {
-    /* position: absolute; */
+    .qualification-label {
+      padding: 0% 1%;
+      background-color: #f6f5f5;
+      margin-left: 2%;
+      position: absolute;
+      top: -9%;
+    }
+    .graduate {
+      h3 {
+        margin-left: 4%;
+      }
+    }
+    .intermediate {
+      h3 {
+        margin-left: 4%;
+      }
+    }
+    .highschool {
+      h3 {
+        margin-left: 4%;
+      }
+    }
   }
 `;
 
