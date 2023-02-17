769ea51442102b434405c0488b5b30ca1a3abda2
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-02-17 18:43:05 +0530

    fix

--- src/components/workshopSubPages/resume/printables/mainFile/MainPrintableFile.js
+++ src/components/workshopSubPages/resume/printables/mainFile/MainPrintableFile.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import { MainPrintStyleDiv } from './mainPrintStyle'
+import React, { useEffect } from "react";
+import { MainPrintStyleDiv } from "./mainPrintStyle";
 
 const MainPrintableFile = () => {
-    
-    const onClickHandler2=()=>{
-        document.getElementById('show-result').style.display="block";
-    }
-    const onClickHandler1=()=>{
-        document.getElementById('show-result').style.display="none";
-    }
+  // const onClickHandler2=()=>{
+  //     document.getElementById('show-result').style.display="block";
+  // }
+  const onClickHandler1 = () => {
+    document.getElementById("show-result").style.display = "none";
+  };
+
+  const Print = () => {
+    //console.log('print');
+    let printContents = document.getElementById("section-to-print").innerHTML;
+    let originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  };
+
+  // useEffect(() => {});j
+  var getQuoteButton = document.getElementById("printBtn");
+  getQuoteButton.addEventListener("click", Print);
+
   return (
     <MainPrintStyleDiv>
-        <div className="vanish-click" onClick={onClickHandler1}>
-        </div>
-        <div className="resume-print">
-            print your file here -
-        </div>
-
+      <div className="vanish-click" onClick={onClickHandler1}></div>
+      <div id="section-to-print" className="resume-print">
+        print your file here -
+      </div>
+      <button id="printBtn">print me</button>
     </MainPrintStyleDiv>
-  )
-}
+  );
+};
 
-export default MainPrintableFile
+export default MainPrintableFile;
--- src/components/workshopSubPages/resume/printables/mainFile/mainPrintStyle.js
+++ src/components/workshopSubPages/resume/printables/mainFile/mainPrintStyle.js
@@ -1,41 +1,72 @@
 import styled from "styled-components";
 
-export const MainPrintStyleDiv=styled.div`
-    position: absolute;
+export const MainPrintStyleDiv = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  .vanish-click {
+    position: fixed;
     width: 100%;
     height: 100%;
-    top: 0;
-    .vanish-click{
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        background-color: black;
-        overflow: auto;
-        z-index: 25;
-        animation: print-1 0.5s linear 0s 1;
-    }
-  
-    .resume-print{
-        position: absolute;
-        top: 0;
-        z-index: 30;
-        width: 80%;
-        height: 153%;
-        left: 10%;
-        color: black;
-        background-color: whitesmoke;
-        margin-top: 2%;
-        margin-bottom: 2%;
-        animation: print-1 0.5s linear 0s 1;
+    background-color: black;
+    z-index: 25;
+    animation: print-1 0.5s linear 0s 1;
+  }
 
+  .resume-print {
+    position: relative;
+    z-index: 30;
+    width: 80%;
+    height: 153%;
+    left: 10%;
+    color: black;
+    background-color: whitesmoke;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    animation: print-1 0.5s linear 0s 1;
+  }
+  button {
+    position: relative;
+    z-index: 26;
+    margin-bottom: 2%;
+    width: 10%;
+    margin-left: 45%;
+  }
+  @keyframes print-1 {
+    0% {
+      opacity: 0;
     }
-    @keyframes print-1 {
-        0%{
-            opacity: 0;
-        }   
-        100%{
-            opacity: 1;
-        }
+    100% {
+      opacity: 1;
     }
+  }
+  /* @media print {
+    .vanish-click {
+      display: none;
+    }
+    .resume-print {
+      margin-top: 0;
+      left: 0;
+      margin-bottom: 0;
+      margin-right: 0;
+      width: 95%;
+      height: 96%;
+      border: 1px solid black;
+    }
+  }
 
-`
+  @media print {
+    body * {
+      visibility: hidden;
+    }
+    #section-to-print * {
+      visibility: visible;
+    }
+    #section-to-print {
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  } */
+`;
