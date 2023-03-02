638f5c2560d969397aa7128320ed9917eea41cd0
kunalsjaswal <kunal23singh0@gmail.com>
2023-03-02 21:44:09 +0530

    schedule builder starting

--- src/components/workshopSubPages/schedule/ScheduleBuilder.jsx
+++ src/components/workshopSubPages/schedule/ScheduleBuilder.jsx
@@ -1,7 +1,86 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { CssTextField } from "../../orangeTextBox/CssTextField";
+import { ScheduleStyleDiv } from "./scheduleStyle";
 
 const ScheduleBuilder = () => {
-  return <div>ScheduleBuilder</div>;
+  const seq=[1,2,3,4,5,6];
+  const task=["I want ot complete my Assignments till MOnday.","I need to take a nap","I need to watch a movie","I will go to the restaurant for dinner","I need to watch Cricket match","I need to complete my project"];
+
+  return( 
+  <ScheduleStyleDiv>
+    <section className="sMain">
+       
+        <div className="left">
+
+          <div className="subheads">
+            Schedule Builder 
+          </div>
+
+          <div className="user-input">
+              <h4 className="schedule-intro">
+                  It helps you to build your whole day Schedule with ease.
+              </h4>
+
+              {/* box  */}
+
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": {
+                    m: 1,
+                    width: "25ch",
+                    background: "white",
+                    borderRadius: "0.8ch",
+                  },
+                  "& .MuiButton-root": { m: 1, width: "30ch", height: "6.5ch" },
+                }}
+                noValidate
+                autoComplete="off"
+                id="select-2"
+                style={{ display: "none" }}
+              >
+
+                </Box>
+
+          </div>
+
+        </div>
+
+        {/* a4 size template  */}
+        <div className="right" id="right-part">
+
+          <div className="generate-tables">
+                <section className="table-head">
+                  <b className="b1">Sequence</b>
+                  <b>Task</b>
+                  <b className="b3">asd</b>
+                </section>
+                
+                {
+                  seq.map((val,indx)=>(
+                    <>
+                    <section className="content" key={indx}>
+                      <div>{val}</div>
+                      <div className="tasks">{task[indx]}</div>
+                      <button>X</button>                    
+                    </section>
+                    <hr style={{margin:"0"}}/>
+                    </>
+                  ))
+                }
+                
+                <section className="bottom">
+                   <div></div>
+                   <div></div>
+                   <div></div>
+                </section>
+          </div>
+
+          
+        </div>
+      </section>
+  </ScheduleStyleDiv>);
 };
 
 export default ScheduleBuilder;
---
+++ src/components/workshopSubPages/schedule/scheduleStyle.js
@@ -0,0 +1,159 @@
+import styled from "styled-components";
+
+export const ScheduleStyleDiv=styled.div`
+    
+section.sMain {
+
+    display: grid;
+    grid-template-columns: 50% 50%;
+    /* border: 1px solid black; */
+    position: absolute;
+    width: 98%;
+    height: 83%;
+    margin-left: 1%;
+    margin-top: -2.5%;
+
+    
+    div.left {
+      position: relative;
+      padding: 2%;
+      height: 85%;
+      background: #f7f7f7;
+      border: 5px solid #595451;
+      border-top: 40px solid #595451;
+      border-radius: 10px;
+      
+      div.user-input {
+        width: 100%;
+        position: relative;
+        border-radius: 15px;
+        margin-top: -2%;
+        /* border: 1px solid black; */
+
+          .schedule-intro {
+            /* border: 1px solid black; */
+            width: 70%;
+            color: #565555;
+            margin: auto;
+            margin-top: 3%;
+            text-align: center;
+          }
+
+        }
+      }
+
+      div.subheads {
+        position: absolute;
+        font-weight: bold;
+        text-shadow: 1px 1px 2px black;
+        color: white;
+        text-align: left;
+        padding-left: 2%;
+        width: 100%;
+        height: 30px;
+        top: -30px;
+        left: 0;
+        background-color: transparent;
+      }
+    }
+
+    div.left::before {
+      position: absolute;
+      content: "h";
+      color: transparent;
+      width: 100%;
+      border-radius: 8px;
+      height: 20px;
+      left: 0%;
+      top: -38px;
+      opacity: 0.7;
+      background-color: #958e8c;
+    }
+  
+
+  div.right {
+    padding: 2%;
+    position: relative;
+    border: 1px solid black;
+
+    .generate-tables{
+
+        .table-head{
+            position: relative;
+            display: grid;
+            grid-template-columns: 15% 75% 10%;
+            text-align: center;
+            align-items: center;
+            color: white;
+            border-radius: 5px;
+            border-bottom-left-radius: 0px;
+            border-bottom-right-radius: 0px;
+            background-color: #565555;
+            b{
+                padding: 3%;
+            }
+            .b1{
+                border-right: 1px solid white;
+            }
+            .b3{
+                border-left: 1px solid white;
+                color: transparent;
+            }
+        }
+
+        /* .table-head::before{
+            content: "1";
+            position: absolute;
+            width: 99%;
+            height: 47%;
+            top: 5%;
+            left: 0.5%;
+            border-radius: 8px;
+            background-color: #837c7a;
+            z-index: 2;
+            color: transparent;
+        } */
+
+        .content{
+            display: grid;
+            grid-template-columns: 15% 75% 10%;
+            text-align: center;
+            align-items: center;
+            color: white;
+            border-radius: 0px;
+            background-color: #43cb4a;
+            border-top-right-radius: 0px;
+            border-top-left-radius: 0px;
+
+            div{
+                padding: 2%;
+            }
+            button{
+                border: 0;
+                width: 70%;
+                padding: 10% 0%;
+                margin: auto;
+                border-radius: 5px;
+                background-color: #f82a2a;
+                color: white;
+                cursor: pointer;
+            }
+            
+        }
+        .bottom{
+            display: grid;
+            grid-template-columns: 15% 75% 10%;
+            text-align: center;
+            align-items: center;
+            color: white;
+            height: 30px;
+            border-radius: 5px;
+            background-color: #4a4747;
+            border-top-right-radius: 0px;
+            border-top-left-radius: 0px;
+        }
+    }
+
+    
+  }
+`
