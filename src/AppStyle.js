d19611c023709521b76fa6ef95050de3a99a61f8
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-04 15:38:43 +0530

    solving css issue

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-export const AppDiv=styled.div`
-    
-    @media (max-width:820px) {
-    }
+export const AppDiv = styled.div`
+  @media (max-width: 820px) {
+  }
 
-    @media (max-width:520px) {
-    }
+  @media (max-width: 520px) {
+  }
 `;
--- src/components/workshopSubPages/aura/auraStyle.jsx
+++ src/components/workshopSubPages/aura/auraStyle.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const AuraContainer = styled.div`
   div {
     font-size: 2em;
+    
     l
   }
 `;
