d5974d3bd18213fec7bdf4d476a44cd2c39ec30a
kunalsjaswal <kunal23singh0@gmail.com>
2023-04-05 10:39:24 +0530

    abs

--- src/components/workshopSubPages/aura/AuraBuilder.jsx
+++ src/components/workshopSubPages/aura/AuraBuilder.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { AuraContainer } from "./auraStyle";
 
 const AuraBuilder = () => {
   return (
-    <AuraContainer>
       <div className="auraMainDiv">Hello aura</div>
-    </AuraContainer>
   );
 };
+
 
 export default AuraBuilder;
--- src/components/workshopSubPages/aura/auraStyle.jsx
+++ src/components/workshopSubPages/aura/auraStyle.jsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const AuraContainer = styled.div`
   div {
     font-size: 2em;
-    l
+    
   }
 `;
