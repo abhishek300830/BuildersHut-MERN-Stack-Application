3c3fd796845ebf5013ab59f6ab34e7218a33ff90
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-05 14:10:37 +0530

    adding design png

--- src/components/workshopSubPages/aura/auraStyle.jsx
+++ src/components/workshopSubPages/aura/auraStyle.jsx
@@ -2,8 +2,5 @@ import styled from "styled-components";
 
 export const AuraContainer = styled.div`
   div {
-    font-size: 2em;
-    
-    l
   }
 `;
--- src/components/workshopSubPages/chart/chartStyle.js
+++ src/components/workshopSubPages/chart/chartStyle.js
@@ -1,32 +1,30 @@
 import styled from "styled-components";
 
-export const ChartStyleDiv=styled.div`
-    img.header{
-        width: 20%;
-        position: absolute;
-        top: 2%;
-        left: 2%;
-    }
+export const ChartStyleDiv = styled.div`
+  img.header {
+    width: 20%;
+    position: absolute;
+    top: 2%;
+    left: 2%;
+  }
 
-
-    /* media queries ####### */
-    @media (max-width:1280px){
-        
+  /* media queries ####### */
+  @media (max-width: 1280px) {
+  }
+  @media (max-width: 820px) {
+    img.header {
+      width: 30%;
+      position: absolute;
+      top: 0.3%;
+      left: 1%;
     }
-    @media (max-width:820px){
-        img.header{
-            width: 30%;
-            position: absolute;
-            top: 0.3%;
-            left: 1%;
-        }
+  }
+  @media (max-width: 520px) {
+    img.header {
+      width: 70%;
+      position: relative;
+      margin-left: 12%;
+      margin-top: -5%;
     }
-    @media (max-width:520px){
-        img.header{
-            width: 70%;
-            position: relative;
-            margin-left: 12%;
-            margin-top: -5%;
-        }
-    }
+  }
 `;
