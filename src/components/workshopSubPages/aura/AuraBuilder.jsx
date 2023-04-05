ceac37430a538ca7cda675ac3b37c732789a8ea6
kunalsjaswal <kunal23singh0@gmail.com>
2023-04-05 14:23:09 +0530

    bug

--- src/components/workshopSubPages/aura/AuraBuilder.jsx
+++ src/components/workshopSubPages/aura/AuraBuilder.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import yogaImage from '../../../images/auraBuilder/yoga.png'
 import { AuraContainer } from "./auraStyle";
+import style1 from '../../../images/auraBuilder/design1.png'
+import style2 from '../../../images/auraBuilder/design2.png'
+import style3 from '../../../images/auraBuilder/design3.png'
+import style4 from '../../../images/auraBuilder/design4.png'
 
 const AuraBuilder = () => {
   return (
     <AuraContainer>
         <section>
-          <div className="top-left"></div>
-          <div className="top-right"></div>
-          <div className="bottom-left"></div>
-          <div className="bottom-right"></div>
+          <img src={style1} alt="anahata" className="top-left common"/>
+          <img src={style2} alt="muladhara" className="top-right common"/>
+          <img src={style3} alt="manipura" className="bottom-left common"/>
+          <img src={style4} alt="ajna" className="bottom-right common"/>
         </section>
           <img src={yogaImage} alt="person-image" className="yoga-man"/>
         
--- src/components/workshopSubPages/aura/auraStyle.jsx
+++ src/components/workshopSubPages/aura/auraStyle.jsx
@@ -3,63 +3,68 @@ import styled from "styled-components";
 export const AuraContainer = styled.div`
   section {
     position: absolute;
-    /* border: 1px solid black; */
     width: 60%;
     height: 70%;
     margin-top: -4%;
     margin-left: 20%;
-  }
-  div {
-    position: absolute;
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-  }
 
-  .top-left {
-    background-color: #d23d3d;
-    left: 20%;
-    animation: top-chakara-anim 5s ease-in-out 0s infinite;
-  }
-  .top-right {
-    background-color: #52e039;
-    right: 20%;
-    animation: top-chakara-anim 5s ease-in-out 0s infinite;
-  }
-  @keyframes top-chakara-anim {
-    0% {
-      margin-top: 0%;
+    img.common {
+      position: absolute;
+      width: 150px;
+      height: 150px;
+      transition: 0.5ms;
+      border: 1px solid black;
     }
-    50% {
-      margin-top: 2%;
+
+    .top-left {
+      left: 20%;
+      animation: top-chakara-anim 5s ease-in-out 0s infinite;
     }
-    100% {
-      margin-top: 0%;
+    .top-right {
+      right: 20%;
+      animation: top-chakara-anim 5s ease-in-out 0s infinite;
     }
-  }
-  .bottom-left {
-    background-color: #1768da;
-    margin-top: 20%;
-    left: 10%;
-    animation: bottom-chakara-anim 5s ease-in-out 0s infinite;
-  }
-  .bottom-right {
-    background-color: #da0623;
-    margin-top: 20%;
-    right: 10%;
-    animation: bottom-chakara-anim 5s ease-in-out 0s infinite;
-  }
-  @keyframes bottom-chakara-anim {
-    0% {
+    @keyframes top-chakara-anim {
+      0% {
+        margin-top: 0%;
+      }
+      50% {
+        margin-top: 2%;
+      }
+      100% {
+        margin-top: 0%;
+      }
+    }
+    .bottom-left {
       margin-top: 20%;
+      left: 10%;
+      animation: bottom-chakara-anim 5s ease-in-out 0s infinite;
     }
-    50% {
-      margin-top: 22%;
-    }
-    100% {
+    .bottom-right {
       margin-top: 20%;
+      right: 10%;
+      animation: bottom-chakara-anim 5s ease-in-out 0s infinite;
+    }
+    @keyframes bottom-chakara-anim {
+      0% {
+        margin-top: 20%;
+      }
+      50% {
+        margin-top: 22%;
+      }
+      100% {
+        margin-top: 20%;
+      }
+    }
+    .top-left:hover{
+      width: 180px;
+      height: 180px;
     }
   }
+  
+
+  
+
   .yoga-man {
     position: absolute;
     width: 40%;
