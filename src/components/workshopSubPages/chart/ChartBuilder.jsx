47b04a791522ec801348d55da135afed05049d9b
kunalsjaswal <kunal23singh0@gmail.com>
2023-04-07 12:54:11 +0530

    chart

--- src/components/workshopSubPages/chart/ChartBuilder.jsx
+++ src/components/workshopSubPages/chart/ChartBuilder.jsx
@@ -3,66 +3,49 @@ import { ChartStyleDiv } from "./chartStyle";
 import chartH from '../../../images/headers/chart.png'
 import ApexCharts from "apexcharts";
 import { useEffect } from "react";
+import CSVReader from "react-csv-reader";
+import { useState } from "react";
 
-// csv 
-// import fs from 'fs'
-// import { parse } from "csv-parse";
-const fs = require('fs')
-const {parse} = require("csv-parse")
-var data=[]
-
-
-fs.createReadStream("../../../csvFiles/PlayTennis.csv")
-  .pipe(parse({delimiter:",",columns:true,ltrim:true})
-    ).on("data", (row)=>{
-      data.push(row)
-    })
-    .on("error", (error)=>{
-      console.log(error.message)
-    })
-    .on("end", ()=>{
-        console.log("parsed csv Data")
-        console.log(data)
-    })
-    
-
-const ChartBuilder = () => {
-
-  // const res = d3.csvParse("Name,Age\nHarry,32\nChris,37");
-  // console.log(res);
-
-  // csv functionality
+const ChartBuilder = () => {    
   
-  
+  const [csvData, setCsvData] = useState()
+  useEffect(()=>{
+    console.log(csvData)
+    chart.render()
+  },)
 
   var options = {
     chart: {
-      type: 'bar'
+      type: 'line'
     },
-    series: [
-      {
-        name: 'sales',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-      }
-    ],
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
     xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
     }
   }
-  
-  var chart = new ApexCharts(document.querySelector('#chart'), options)
-  
-  useEffect(() => {
-    chart.render()
-  }, )
-  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);  
+
+
 
   return (
   <ChartStyleDiv>
 
     {/* header  */}
+
     <img src={chartH} alt="header" className="header"/>
-    <div id="chart"></div>
+    <div>
+      {
+        <CSVReader
+        parserOptions={{ header: true }}
+        onFileLoaded={(data, fileInfo) => setCsvData(data)}
+      />
+    }
+    </div>
+    <div id="chart" style={{width:"50%", border:'2px solid black',marginTop:'2%'}}></div>
+
 
   </ChartStyleDiv>
   );
