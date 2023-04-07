5f1ba326a2aa801f2757e5fb5a496e7458f81a4b
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-07 14:19:14 +0530

    added 3 charts

--- src/components/workshopSubPages/chart/ChartBuilder.jsx
+++ src/components/workshopSubPages/chart/ChartBuilder.jsx
@@ -1,68 +1,146 @@
-import React ,{ useEffect,useState }from "react";
+import React, { useEffect, useState } from "react";
 import { ChartStyleDiv } from "./chartStyle";
-import chartH from '../../../images/headers/chart.png'
+import chartH from "../../../images/headers/chart.png";
 import ApexCharts from "apexcharts";
 import CSVReader from "react-csv-reader";
 
-const ChartBuilder = () => {    
-  
-  const [csvData, setCsvData] = useState([])
+const ChartBuilder = () => {
+  const [csvData, setCsvData] = useState([]);
 
-  const [attributes, setAttributes] = useState([])
+  const [attributes, setAttributes] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     // if(csvData.length > 0){
     //   console.log(Number.isNaN(Number.parseInt(csvData[0].play)))
     // }
 
-    if(csvData.length>0){
-      setAttributes(Object.keys(csvData[0]))
+    if (csvData.length > 0) {
+      setAttributes(Object.keys(csvData[0]));
       // console.log())
+      var chart = new ApexCharts(document.querySelector("#chart"), options);
+      var pieChart = new ApexCharts(
+        document.querySelector("#pieChart"),
+        optionsPie
+      );
+      var columnChart = new ApexCharts(
+        document.querySelector("#columnChart"),
+        optionsColumn
+      );
+      chart.render();
+      pieChart.render();
+      columnChart.render();
     }
+  }, [csvData]);
 
-    
-    // chart.render()
-  },[csvData])
-
+  //  line chart
   var options = {
     chart: {
-      type: 'line'
+      type: "line",
     },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
-  }
-  var chart = new ApexCharts(document.querySelector("#chart"), options);  
-
-  return (
-  <ChartStyleDiv>
-
-    {/* header  */}
-
-    <img src={chartH} alt="header" className="header"/>
-    <div>
+    series: [
       {
-        <CSVReader
-        parserOptions={{ header: true }}
-        onFileLoaded={(data, fileInfo) => setCsvData(data)}
-      />
-    }
-    </div>
+        name: "sales",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  };
 
-    {attributes.length>0 && 
-    attributes.map((val,indx)=>(
-      <div key={indx}>
-          {indx+1}. {val} - {(Number.isNaN(Number.parseInt(csvData[0][val])))?'String':'Number'}
+  // Pie Chart
+
+  var optionsPie = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+      type: "donut",
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+  // Column charts
+  var optionsColumn = {
+    series: [
+      {
+        data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        "South Korea",
+        "Canada",
+        "United Kingdom",
+        "Netherlands",
+        "Italy",
+        "France",
+        "Japan",
+        "United States",
+        "China",
+        "Germany",
+      ],
+    },
+  };
+  return (
+    <ChartStyleDiv>
+      {/* header  */}
+
+      <img src={chartH} alt="header" className="header" />
+      <div>
+        {
+          <CSVReader
+            parserOptions={{ header: true }}
+            onFileLoaded={(data, fileInfo) => setCsvData(data)}
+          />
+        }
       </div>
-    ))}
-    {/* <div id="chart" style={{width:"50%", border:'2px solid black',marginTop:'2%'}}></div> */}
 
-
-  </ChartStyleDiv>
+      {attributes.length > 0 &&
+        attributes.map((val, indx) => (
+          <div key={indx}>
+            {indx + 1}. {val} -{" "}
+            {Number.isNaN(Number.parseInt(csvData[0][val]))
+              ? "String"
+              : "Number"}
+          </div>
+        ))}
+      <div
+        id="chart"
+        style={{ width: "20%", border: "2px solid black", marginTop: "2%" }}
+      ></div>
+      <div
+        id="pieChart"
+        style={{ width: "20%", border: "2px solid black", marginTop: "2%" }}
+      ></div>
+      <div
+        id="columnChart"
+        style={{ width: "20%", border: "2px solid black", marginTop: "2%" }}
+      ></div>
+    </ChartStyleDiv>
   );
 };
 
