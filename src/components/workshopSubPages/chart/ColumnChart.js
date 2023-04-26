14ad9d86917a3995ae1d49ebbf5f4650ce4f091d
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-26 11:04:44 +0530

    xyz

--- src/components/workshopSubPages/chart/ColumnChart.js
+++ src/components/workshopSubPages/chart/ColumnChart.js
@@ -1,67 +1,78 @@
-import React, { useEffect } from 'react'
-import ApexCharts from 'apexcharts'
+import React, { useEffect } from "react";
+import ApexCharts from "apexcharts";
 
-const ColumnChart = ({csvData,data}) => {
+const ColumnChart = ({ csvData, data }) => {
+  var seriesDataArr = [];
+  useEffect(() => {
+    csvData.forEach((val) => {
+      let num = parseInt(val[data[0]]);
+      if (!isNaN(num)) {
+        seriesDataArr.push(num);
+      }
+      // console.log("val", val[data[0]]);
+    });
 
-    useEffect(()=>{
-        var columnChart = new ApexCharts(
-            document.querySelector("#columnChart"),
-            optionsColumn
-          );
+    var columnChart = new ApexCharts(
+      document.querySelector("#columnChart"),
+      optionsColumn
+    );
+    columnChart.render();
+  }, []);
 
-        columnChart.render();
-        
-    },[])
+  console.log("data", data);
+  console.log("csvData", csvData);
 
-    console.log(data)
-
-    var optionsColumn = {
-        series: [
-          {
-            data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
-          },
-        ],
-        chart: {
-          type: "bar",
-          height: 350,
-        },
-        plotOptions: {
-          bar: {
-            borderRadius: 4,
-            horizontal: true,
-          },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        xaxis: {
-          categories: [
-            "South Korea",
-            "Canada",
-            "United Kingdom",
-            "Netherlands",
-            "Italy",
-            "France",
-            "Japan",
-            "United States",
-            "China",
-            "Germany",
-          ],
-        },
-      };
-
-
+  var optionsColumn = {
+    series: [
+      {
+        // data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+        data: seriesDataArr,
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      // categories: [
+      //   "South Korea",
+      //   "Canada",
+      //   "United Kingdom",
+      //   "Netherlands",
+      //   "Italy",
+      //   "France",
+      //   "Japan",
+      //   "United States",
+      //   "China",
+      //   "Germany",
+      // ],
+      catagories: csvData.map((val) => {
+        // console.log("data[1]", data[1]);
+        // console.log("Valdata[1]", val[data[1]]);
+        return val[data[1]];
+      }),
+    },
+  };
 
   return (
     <div
-        id="columnChart"
-        style={{
+      id="columnChart"
+      style={{
         width: "100%",
         border: "2px solid black",
         marginTop: "2%",
-        }}
+      }}
     ></div>
-  )
-}
+  );
+};
 
-export default ColumnChart
+export default ColumnChart;
